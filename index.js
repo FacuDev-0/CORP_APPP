9eeea405d10aabba99c03eba8d780afa2d98649a
Facundo Silvera <facusilvera1410@outlook.com>
2023-07-29 03:15:45 -0300

    add start-package.json

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import appRouter from './router/appRouter.js' // Atencion
 import dataBase from './config/db.js'
 import cors from "cors"
 import dotenv from 'dotenv/config'
-import fs from 'fs'
 
-// console.log(fs.existsSync('../backend/router/inicioRouter.js'))
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
